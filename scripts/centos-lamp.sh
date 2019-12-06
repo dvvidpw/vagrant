958fe08d6a925c7d4d6bae2e9c22904a3d5fb8e3
David Watson <david.watson@ymcacolumbus.org>
2019-12-06 16:14:56 -0500

    updated LAMP script with github files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -30,4 +30,8 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 # Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/dvvidpw/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/dvvidpw/vagrant/master/files/info.php
+
 service httpd restart
